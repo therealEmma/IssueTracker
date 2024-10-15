ae9ef2208bbe8c7ddd30f4207cda55e4d676fad9
therealEmma <eozegbe68@gmail.com>
2024-10-15 09:56:12 +0100

    Handling Errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -7,47 +7,51 @@ import axios from "axios";
 import { useForm, Controller } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { z } from 'zod';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/api/issues/Validation";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm= z.infer<typeof createIssueSchema>
 
 const NewIssuesPage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
-  const [error, setError] = useState('');
+  const { register, control, handleSubmit } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema)
+  });
+  const [error, setError] = useState("");
 
   // This function is called when the form is submitted
   const onSubmit = async (data: IssueForm) => {
     try {
       // Sending the form data to the API
-      await axios.post('/api/issues', data);
+      await axios.post("/api/issues", data);
 
       // Redirecting the user to the /issues page after successful submission
-      router.push('/issues');
+      router.push("/issues");
     } catch (error) {
       // You can handle any error here, like showing an alert
-      setError('An unexpected error occured');
+      setError("An unexpected error occured");
     }
   };
 
   return (
     <div className="max-w-xl">
-      {error && (<Callout.Root color="red" className="mb-5">
-        <Callout.Text>{error}</Callout.Text>
-      </Callout.Root>)}
-    <form className="space-y-3" onSubmit={handleSubmit(onSubmit)}>
-      <TextField.Root placeholder="Title..." {...register("title")} />
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description…" {...field} />
-        )}
-      />
-      <Button type="submit">Submit New Issue</Button>
-    </form>
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className="space-y-3" onSubmit={handleSubmit(onSubmit)}>
+        <TextField.Root placeholder="Title..." {...register("title")} />
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description…" {...field} />
+          )}
+        />
+        <Button type="submit">Submit New Issue</Button>
+      </form>
     </div>
   );
 };
